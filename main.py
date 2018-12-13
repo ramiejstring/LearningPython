06362490e11305e5ebfdd29610442610702713c2
Jei <ramiejstring@gmail.com>
2018-12-13 14:03:28 +0800

    Create main.py
    Python Homework #1 - Variables

---
+++ main.py
@@ -0,0 +1,29 @@
+"""
+Below are the information about my favorite song.
+This song is not getting old anytime soon.
+"""
+
+#title of my favorite song (string)
+Title = "A Hard Day's Night"
+#band who performed the song (string)
+Artist = "The Beatles"
+#album name (string)
+Album = "A Hard Day's Night Deluxe"
+#file size in MB (float)
+SizeInMB = 6.46
+
+FileType = "MP3"
+Year = 1964
+TrackNumber = 7
+Genre = "Rock"
+Publisher = "Capitol Records"
+
+print(Title)
+print(Artist)
+print(Album)
+print(SizeInMB)
+print(FileType)
+print(Year)
+print(TrackNumber)
+print(Genre)
+print(Publisher)
