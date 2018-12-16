e5845b03afa740d24287a5d336e861d253107346
Jei <ramiejstring@gmail.com>
2018-12-16 08:55:22 +0800

    Create "if statement.py"

---
+++ if statement.py
@@ -0,0 +1,15 @@
+#assign values
+a = 4
+b = 3
+c = "3"
+
+atleast2ValuesAreEqual = False
+
+#check data type
+
+
+#check if atleast 2 values are equal
+if a == b or a == int(c) or b == int(c):
+    atleast2ValuesAreEqual = True
+
+print(atleast2ValuesAreEqual)
